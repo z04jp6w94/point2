fa9e5ad0dc8cdb8ddf2b9e4dfdec32e9dcbc2cfd
SaSaChu <teresa7397@gmail.com>
2018-09-28 17:18:27 +0800

    Minify js、html, fix gh-pages path bug.

--- js/l10n.js
+++ js/l10n.js
@@ -1,18 +1,1 @@
-// Simple localization
-const isGithubPages = window.location.hostname === 'stripe.github.io';
-const localeIndex = isGithubPages ? 2 : 1;
-window.__exampleLocale = window.location.pathname.split('/')[localeIndex] || 'en';
-const urlPrefix = isGithubPages ? '/elements-examples/' : '/';
-
-document.querySelectorAll('.optionList a').forEach(function(langNode) {
-  const langValue = langNode.getAttribute('data-lang');
-  const langUrl = langValue === 'en' ? urlPrefix : (urlPrefix + langValue + '/');
-
-  if (langUrl === window.location.pathname || langUrl === window.location.pathname + '/') {
-    langNode.className += ' selected';
-    langNode.parentNode.setAttribute('aria-selected', 'true');
-  } else {
-    langNode.setAttribute('href', langUrl);
-    langNode.parentNode.setAttribute('aria-selected', 'false');
-  }
-});
+const isGithubPages="stripe.github.io"===window.location.hostname,localeIndex=isGithubPages?2:1;window.__exampleLocale=window.location.pathname.split("/")[localeIndex]||"en";const urlPrefix=isGithubPages?"/elements-examples/":"/";document.querySelectorAll(".optionList a").forEach(function(e){const t=e.getAttribute("data-lang"),a="en"===t?urlPrefix:urlPrefix+t+"/";a===window.location.pathname||a===window.location.pathname+"/"?(e.className+=" selected",e.parentNode.setAttribute("aria-selected","true")):(e.setAttribute("href",a),e.parentNode.setAttribute("aria-selected","false"))});
